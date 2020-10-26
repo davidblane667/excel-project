c343c53b06bdb96ddbf4e3a1fd2c1e13f53c2356
Nikita <davidblane667@mail.ru>
2020-10-26 22:01:11 +0700

    Finish table resize

--- src/core/dom.js
+++ src/core/dom.js
@@ -40,6 +40,36 @@ class Dom {
 
     return this
   }
+
+  get data() {
+    return this.$el.dataset
+  }
+
+  closest(selector) {
+    return $(this.$el.closest(selector))
+  }
+
+  getCoords() {
+    return this.$el.getBoundingClientRect()
+  }
+
+  findAll(selector) {
+    return this.$el.querySelectorAll(selector)
+  }
+
+  css(styles = {}) {
+    Object
+      .keys(styles)
+      .forEach(key => this.$el.style[key] = styles[key])
+  }
+
+  addClass(selector) {
+    return this.$el.classList.add(selector)
+  }
+
+  removeClass(selector) {
+    return this.$el.classList.remove(selector)
+  }
 }
 
 
